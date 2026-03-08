04aad2df679f435a212673d6eb6131608a037446
schwartz <gtfsolo@gmail.com>
2026-03-08 23:44:52 +0200

    search todo finished

--- src/components/templates/Header/Header.tsx
+++ src/components/templates/Header/Header.tsx
@@ -2,22 +2,63 @@ import { useInput } from "@shared/hooks/useInput";
 import { useDebounceDispatch } from "./model/useDebouncedDispatch";
 import style from "./Header.module.scss";
 
+import { FiSearch, FiX } from "react-icons/fi";
+import { useRef } from "react";
+
 export function Header() {
   const search = useInput("");
   useDebounceDispatch(search.value, 400);
 
-  return (
-    <header className="header">
-      <div className="header__container">
-        <div>USA</div>
+  const inputRef = useRef<HTMLInputElement>(null);
+  const clearSearch = () => search.onReset();
 
-        <input
-          type="text"
-          {...search}
-          placeholder="Search todo..."
-          className={style.search}
-        />
+  const focusInput = () => {
+    inputRef.current?.focus();
+  };
+
+  return (
+    <header className={style.header}>
+      <div className="header__container">
+        <div className={style.box}>
+          <div>UA</div>
+
+          <form className={style.search} onSubmit={(e) => e.preventDefault()}>
+            <label htmlFor="todo-search" className="visually-hidden">
+              Search todo
+            </label>
+
+            <div className={style.searchField}>
+              <FiSearch
+                className={style.searchIcon}
+                onClick={focusInput}
+                size={18}
+              />
+
+              <input
+                type="search"
+                id="todo-search"
+                ref={inputRef}
+                disabled={false}
+                className={style.input}
+                placeholder="Search todo..."
+                {...search}
+              />
+
+              {search.value && (
+                <button
+                  type="button"
+                  className={style.clear}
+                  onClick={clearSearch}
+                >
+                  <FiX size={18} />
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
       </div>
     </header>
   );
 }
+
+// А ти може мені показати як зробити варіант де буде autocomplete dropdown ? Я використовую redux
